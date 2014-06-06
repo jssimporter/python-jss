a67038a18b5c23e3649f80b994fbdda57c973db9
Shea Craig <shea.craig@da.org>
2014-06-06 10:03:52 -0400

    Reorganize tests.
    Move test functions into classes per corresponding module class.
    Condense WS.

--- test/jss_test.py
+++ test/jss_test.py
@@ -41,195 +41,204 @@ def setup():
         pass
 
 
-#JSSPrefs Tests################################################################
+class testJSSPrefs(object):
+
+    def test_jssprefs(self):
+        jp = JSSPrefs()
+        result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_user'])
+        assert_in(jp.user, result)
+        result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_pass'])
+        assert_in(jp.password, result)
+        result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_url'])
+        assert_in(jp.url, result)
+
+    def test_jssprefs_missing_file_error(self):
+        assert_raises(JSSPrefsMissingFileError, JSSPrefs, '/nonexistent_path')
+
+    def test_jssprefs_missing_key_error(self):
+        assert_raises(JSSPrefsMissingKeyError, JSSPrefs, 'test/incomplete_preferences.plist')
 
 
-def test_jssprefs():
-    jp = JSSPrefs()
-    result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_user'])
-    assert_in(jp.user, result)
-    result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_pass'])
-    assert_in(jp.password, result)
-    result = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_url'])
-    assert_in(jp.url, result)
+class testJSS(object):
+
+    def test_jss_with_jss_prefs(self):
+        jp = JSSPrefs()
+        j = JSS(jss_prefs=jp)
+        assert_is_instance(j, JSS)
+
+    def test_jss_with_args(self):
+        authUser = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_user'])
+        authPass = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_pass'])
+        repoUrl = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_url'])
+        j = JSS(url=repoUrl, user=authUser, password=authPass)
+        assert_is_instance(j, JSS)
+
+    def test_jss_get_error(self):
+        assert_raises(JSSGetError, j_global.get, '/donkey-tacos')
+
+    def test_jss_get(self):
+        policy = j_global.get(Policy.get_url(None))
+        assert_is_instance(policy, ElementTree.Element)
+
+    @with_setup(setup)
+    def test_jss_post(self):
+        pt = JSSPolicyTemplate()
+        new_policy = j_global.Policy(pt)
+        # If successful, we'll get a new ID number
+        assert_is_instance(new_policy.id(), int)
+        new_policy.delete()
+
+    def test_jss_method_constructors(self):
+        skip_these_methods = ['__init__', 'get', 'delete', 'put', 'post', '_error_handler']
+        method_constructors = [ m[1] for m in inspect.getmembers(j_global) if inspect.ismethod(m[1]) and m[0] not in skip_these_methods]
+        for cls in method_constructors:
+            instance = cls()
+            yield self.check_jss_method_constructor, instance
+
+    def check_jss_method_constructor(self, instance):
+            assert_true(isinstance(instance, JSSObject) or isinstance(instance, JSSObjectList))
+
+    @with_setup(setup)
+    def test_jss_put(self):
+        pt = JSSPolicyTemplate()
+        new_policy = j_global.Policy(pt)
+        id_ = new_policy.id()
+
+        # Change the policy.
+        recon = new_policy.find('maintenance/recon')
+        # This is str, not bool...
+        recon.text = 'false'
+        new_policy.update()
+
+        test_policy = j_global.Policy(id_)
+        assert_equal(test_policy.find('maintenance/recon').text, 'false')
+
+        new_policy.delete()
+
+    @with_setup(setup)
+    def test_jss_delete(self):
+        pt = JSSPolicyTemplate()
+        new_policy = j_global.Policy(pt)
+        # If successful, we'll get a new ID number
+        assert_is_instance(new_policy.id(), int)
+        id_ = new_policy.id()
+
+        # Test delete. This is of course successful if the previous two tests
+        # pass.
+        new_policy.delete()
+        assert_raises(JSSGetError, j_global.Policy, id_)
 
 
-def test_jssprefs_missing_file_error():
-    assert_raises(JSSPrefsMissingFileError, JSSPrefs, '/nonexistent_path')
+class testJSSObject(object):
+    def test_jssobject_unsupported_search_method_error(self):
+        assert_raises(JSSUnsupportedSearchMethodError,
+                      j_global.Policy, 'taco=alpastor')
+
+    def test_JSSObject_get_url(self):
+        assert_equal(Policy.get_url(None), '/policies')
+        assert_equal(Policy.get_url(42), '/policies/id/42')
+        assert_equal(Policy.get_url('taco'), '/policies/name/taco')
+        assert_equal(Computer.get_url('taco'), '/computers/match/taco')
+        assert_equal(Computer.get_url('udid=taco'), '/computers/udid/taco')
+
+    def test_JSSObject_get_post_url(self):
+        assert_equal(Policy.get_post_url(), '/policies/id/0')
+
+    def test_JSSObject_get_object_url(self):
+        pt = JSSPolicyTemplate()
+        new_policy = j_global.Policy(pt)
+
+        assert_equal(new_policy.get_object_url(), '/policies/id/%s' % 
+                     new_policy.id())
+
+        new_policy.delete()
+
+    def test_jssobject_method_not_allowed(self):
+        # This type of object probably doesn't exist in the wild.
+        class NoListObject(JSSObject):
+            can_list = False
+            can_get = False
+
+        class NoGetObject(JSSObject):
+            can_get = False
+
+        class NoPostObject(JSSObject):
+            can_post = False
+
+        class NoPutObject(JSSObject):
+            can_put = False
+            def __init__(self):
+                pass
+
+        class NoDeleteObject(JSSObject):
+            can_delete = False
+            def __init__(self):
+                pass
+
+        assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
+                      NoListObject, None)
+        assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
+                      NoGetObject, None)
+        bad_element = ElementTree.fromstring("<xml>No workie.</xml>")
+        bad_policy = JSSObjectTemplate(element=bad_element)
+        assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
+                      NoPostObject, bad_policy)
+
+        np = NoPutObject()
+        assert_raises(JSSMethodNotAllowedError, np.update)
+
+        nd = NoDeleteObject()
+        assert_raises(JSSMethodNotAllowedError, nd.delete)
 
 
-def test_jssprefs_missing_key_error():
-    assert_raises(JSSPrefsMissingKeyError, JSSPrefs, 'test/incomplete_preferences.plist')
+class testJSSObjectFactory(object):
+    def test_JSSObjectFactory_list(self):
+        obj_list = j_global.factory.get_object(Policy)
+        assert_is_instance(obj_list, JSSObjectList)
+
+    def test_JSSObjectFactory_JSSObject(self):
+        obj_list = j_global.factory.get_object(Policy, 242)
+        assert_is_instance(obj_list, Policy)
 
 
-#JSS Tests#####################################################################
-
-
-def test_jss_with_jss_prefs():
-    jp = JSSPrefs()
-    j = JSS(jss_prefs=jp)
-    assert_is_instance(j, JSS)
-
-
-def test_jss_with_args():
-    authUser = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_user'])
-    authPass = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_pass'])
-    repoUrl = subprocess.check_output(['defaults', 'read', 'org.da.jss_helper', 'jss_url'])
-    j = JSS(url=repoUrl, user=authUser, password=authPass)
-    assert_is_instance(j, JSS)
-
-
-def test_jss_get_error():
-    assert_raises(JSSGetError, j_global.get, '/donkey-tacos')
-
-
-def test_jss_get():
-    policy = j_global.get(Policy.get_url(None))
-    assert_is_instance(policy, ElementTree.Element)
-
-
-@with_setup(setup)
-def test_jss_post():
-    pt = JSSPolicyTemplate()
-    new_policy = j_global.Policy(pt)
-    # If successful, we'll get a new ID number
-    assert_is_instance(new_policy.id(), int)
-    new_policy.delete()
-
-
-def test_jss_method_constructors():
-    skip_these_methods = ['__init__', 'get', 'delete', 'put', 'post', '_error_handler']
-    method_constructors = [ m[1] for m in inspect.getmembers(j_global) if inspect.ismethod(m[1]) and m[0] not in skip_these_methods]
-    for cls in method_constructors:
-        instance = cls()
-        assert_true(isinstance(instance, JSSObject) or isinstance(instance, JSSObjectList), msg='The %s was not expected as a type.' % cls)
-
-
-# These test both JSS methods and JSSObject methods. I don't feel the need to
-# write more to cover both.
-
-
-@with_setup(setup)
-def test_jss_put():
-    pt = JSSPolicyTemplate()
-    new_policy = j_global.Policy(pt)
-    id_ = new_policy.id()
-
-    # Change the policy.
-    recon = new_policy.find('maintenance/recon')
-    # This is str, not bool...
-    recon.text = 'false'
-    new_policy.update()
-
-    test_policy = j_global.Policy(id_)
-    assert_equal(test_policy.find('maintenance/recon').text, 'false')
-
-    new_policy.delete()
-
-
-@with_setup(setup)
-def test_jss_delete():
-    pt = JSSPolicyTemplate()
-    new_policy = j_global.Policy(pt)
-    # If successful, we'll get a new ID number
-    assert_is_instance(new_policy.id(), int)
-    id_ = new_policy.id()
-
-    # Test delete. This is of course successful if the previous two tests
-    # pass.
-    new_policy.delete()
-    assert_raises(JSSGetError, j_global.Policy, id_)
-
-
-#JSSObject Tests###############################################################
-#TODO: Test methods of JSSObject
-
-def test_jssobject_unsupported_search_method_error():
-    assert_raises(JSSUnsupportedSearchMethodError,
-                  j_global.Policy, 'taco=alpastor')
-
-
-#TODO: Move to JSSObject subclasses section
-def jssobject_runner(object_cls):
-    """ Helper function to test individual object classes."""
-    obj_list = j_global.factory.get_object(object_cls)
-    assert_is_instance(obj_list, JSSObjectList)
-    # There should be objects in the JSS to test for.
-    assert_greater(len(obj_list), 0)
-    id_ = obj_list[0].id()
-    obj = j_global.factory.get_object(object_cls, id_)
-    assert_is_instance(obj, object_cls, msg='The object of type %s was not '
-                      'expected.' % type(obj))
-
-
-def test_container_JSSObject_subclasses():
-    """Test for factory to return objects of each of our JSSObject
-    subclasses that are containers.
-
-    """
-    objs = [Category, Computer, ComputerGroup, Policy, MobileDevice,
-            MobileDeviceConfigurationProfile, MobileDeviceGroup]
-    for obj in objs:
-        jssobject_runner(obj)
-
-
-def test_jssobject_method_not_allowed():
-    # This type of object probably doesn't exist in the wild.
-    class NoListObject(JSSObject):
-        can_list = False
-        can_get = False
-
-    class NoGetObject(JSSObject):
-        can_get = False
-
-    class NoPostObject(JSSObject):
-        can_post = False
-
-    class NoPutObject(JSSObject):
-        can_put = False
-        def __init__(self):
-            pass
-
-    class NoDeleteObject(JSSObject):
-        can_delete = False
-        def __init__(self):
-            pass
-
-    assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
-                  NoListObject, None)
-    assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
-                  NoGetObject, None)
-    bad_element = ElementTree.fromstring("<xml>No workie.</xml>")
-    bad_policy = JSSObjectTemplate(element=bad_element)
-    assert_raises(JSSMethodNotAllowedError, j_global.factory.get_object,
-                  NoPostObject, bad_policy)
-
-    np = NoPutObject()
-    assert_raises(JSSMethodNotAllowedError, np.update)
-
-    nd = NoDeleteObject()
-    assert_raises(JSSMethodNotAllowedError, nd.delete)
-
-
-#JSSObjectFactory Tests########################################################
-def test_JSSObjectFactory_list():
-    obj_list = j_global.factory.get_object(Policy)
-    assert_is_instance(obj_list, JSSObjectList)
-
-
-def test_JSSObjectFactory_JSSObject():
-    obj_list = j_global.factory.get_object(Policy, 242)
-    assert_is_instance(obj_list, Policy)
-
-
-#JSSDeviceObject Tests#########################################################
-
-#JSSObject Subclasses Tests####################################################
-
-#JSSObjectTemplate Tests#######################################################
-
-#JSSListData Tests#############################################################
-
-#JSSObjectList Tests###########################################################
-def test_JSSObjectList_JSSListIDError():
+class testJSSDeviceObject(object):
     pass
+
+
+class testJSSObject_Subclasses(object):
+    def jssobject_runner(self, object_cls):
+        """ Helper function to test individual object classes."""
+        obj_list = j_global.factory.get_object(object_cls)
+        assert_is_instance(obj_list, JSSObjectList)
+        # There should be objects in the JSS to test for.
+        assert_greater(len(obj_list), 0)
+        id_ = obj_list[0].id()
+        obj = j_global.factory.get_object(object_cls, id_)
+        assert_is_instance(obj, object_cls, msg='The object of type %s was not '
+                          'expected.' % type(obj))
+
+    def test_container_JSSObject_subclasses(self):
+        """Test for factory to return objects of each of our JSSObject
+        subclasses that are containers.
+
+        """
+        objs = [Category, Computer, ComputerGroup, Policy, MobileDevice,
+                MobileDeviceConfigurationProfile, MobileDeviceGroup]
+        for obj in objs:
+            yield self.jssobject_runner, obj
+
+
+class testJSSObjectTemplate(object):
+    pass
+
+
+class testJSSListData(object):
+    pass
+
+
+class testJSSObjectList(object):
+    def test_JSSObjectList_JSSListIDError(self):
+        policies = j_global.Policy()
+        num_of_policies = len(policies)
+        oops_id = num_of_policies + 42
+        assert_raises(JSSListIDError, policies.retrieve_by_id, oops_id)
