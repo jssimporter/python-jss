9b18ba8bf64123bdc0e1580e0c2ddabcb037f352
Shea Craig <shea.craig@da.org>
2014-06-18 09:47:12 -0400

    md to rst conversion for REAMDE.

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 
 from setuptools import setup, find_packages
 
+#http://stackoverflow.com/questions/10718767/have-the-same-readme-both-in-markdown-and-restructuredtext
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("Warning: pypandoc module not found, could not convert md to rst")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(name='python-jss',
       version='0.2',
       #py_modules=['jss', 'FoundationPlist'],
       packages=find_packages(),
       description='Python wrapper for JSS API.',
-      long_description=open('README.md').read(),
+      long_description=read_md('README.md'),
       author='Shea G. Craig',
       author_email='shea.craig@da.org',
       url='https://github.com/sheagcraig/python-jss/',
