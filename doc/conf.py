5a7db45efacc0eab473c10c668cb851ed8d7bf23
Mosen <mosen1@gmail.com>
2017-03-13 11:45:05 +1100

    Add several auto generated objects. Minor tweaking of docstrings. Added napoleon sphinx ext to parse Google style docstrings.

--- doc/conf.py
+++ doc/conf.py
@@ -21,8 +21,6 @@ import sys
 sys.path.insert(0, os.path.abspath('../'))
 #sys.path.append(os.path.abspath('../'))
 
-print(sys.path)
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -35,6 +33,7 @@ print(sys.path)
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -122,10 +121,12 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
+import sphinx_rtd_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -135,6 +136,7 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
