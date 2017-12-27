45e0c39f2e2d03ccabdd6502028fdfa348129e1d
Mosen <mosen@users.noreply.github.com>
2017-12-27 20:27:53 +1100

    docker based smb distribution point testing. best practice setuptools configuration for pytest.
    (cherry picked from commit 7285b4d)

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import plistlib
 from jss import JSSPrefs, JSS
 from xml.etree import ElementTree
 from jss.requests_adapter import RequestsAdapter
+from subprocess import call
 
 JSS_PREFS = {
     'jss_url': 'https://localhost:8444',
@@ -15,25 +16,25 @@ JSS_PREFS = {
 
 
 @pytest.fixture
-def jss_prefs_dict():  # () -> dict
+def jss_prefs_dict():  # type: () -> dict
     return JSS_PREFS
 
 
 @pytest.fixture
-def jss_prefs_file(tmpdir):  # () -> str
+def jss_prefs_file(tmpdir):  # type: () -> str
     prefs_path = tmpdir.join('com.github.sheagcraig.python-jss.plist')
     plistlib.writePlist(JSS_PREFS, prefs_path)
     return prefs_path
 
 
 @pytest.fixture
-def jss_prefs(jss_prefs_file):  # (str) -> JSSPrefs
+def jss_prefs(jss_prefs_file):  # type: (str) -> JSSPrefs
     prefs = JSSPrefs(preferences_file=jss_prefs_file)
     return prefs
 
 
 @pytest.fixture
-def j(jss_prefs_dict):  # (dict) -> JSS
+def j(jss_prefs_dict):  # type: (dict) -> JSS
     o = JSS(
         url=jss_prefs_dict['jss_url'],
         user=jss_prefs_dict['jss_user'],
@@ -44,7 +45,7 @@ def j(jss_prefs_dict):  # (dict) -> JSS
 
 
 @pytest.fixture
-def jrequests(jss_prefs_dict):  # (dict) -> JSS
+def jrequests(jss_prefs_dict):  # type: (dict) -> JSS
     o = JSS(
         url=jss_prefs_dict['jss_url'],
         user=jss_prefs_dict['jss_user'],
@@ -62,14 +63,49 @@ def jrequests(jss_prefs_dict):  # (dict) -> JSS
 
 
 @pytest.fixture
-def requests_adapter(jss_prefs_dict):  # () -> RequestsAdapter
+def requests_adapter(jss_prefs_dict):  # type: () -> RequestsAdapter
     adapter = RequestsAdapter(jss_prefs_dict['jss_url'])
     return adapter
 
 @pytest.fixture
-def etree_building():  # () -> ElementTree.Element
+def etree_building():  # type: () -> ElementTree.Element
     building = ElementTree.Element('building')
     name = ElementTree.SubElement(building, 'name')
-    name.text = 'Fixture'
+    name.text = 'Fixture Building'
+    id = ElementTree.SubElement(building, 'id')
+    id.text = '0'
     
     return building
+
+
+# def is_afp_responsive(afpurl):
+#     """Check if something responds to ``url``."""
+#     pass
+
+
+def is_smb_responsive(smburl):
+    """Check if something responds to ``url``."""
+    status = call(['/usr/bin/smbutil', 'view', smburl])
+    return status == 0
+
+
+@pytest.fixture
+def dp_smb_ip_port(docker_ip, docker_services):
+    docker_services.wait_until_responsive(
+        timeout=30.0, pause=0.1,
+        check=lambda: is_smb_responsive("//jss:jss@%s:%s" % (docker_ip, docker_services.port_for('samba', 139)))
+    )
+    return docker_ip, docker_services.port_for('samba', 139)
+
+
+# @pytest.fixture
+# def dp_afp_url(docker_ip, docker_services):
+#     afp_url = 'afp://%s:%s/distribution_point' % (
+#         docker_ip,
+#         docker_services.port_for('afp', 549),
+#     )
+#     docker_services.wait_until_responsive(
+#         timeout=30.0, pause=0.1,
+#         check=lambda: is_afp_responsive(afp_url)
+#     )
+#     return afp_url
