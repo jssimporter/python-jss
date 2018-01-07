90251be4050b8dc5d8109acf20c1977699c0857d
Mosen <mosen@users.noreply.github.com>
2018-01-07 22:13:43 +1100

    api search methods are being attached to the JSS instance for UAPI objects The JSSResource prefix is extracted from JSS' GET/POST/PUT/DELETE and moved into the base jssobject. Attempted and then commented out cookies retrieved from NSURLHTTPResponse Added some basic skeleton objects for the UAPI. Add further object tests for API and UAPI.
    (cherry picked from commit c9be7a9)

---
+++ jss/uapiobjects.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# Copyright (C) 2014-2017 Shea G Craig
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""uapiobjects.py
+
+Classes representing JSS database objects and their UAPI endpoints
+"""
+from .uapiobject import UAPIObject, UAPIContainer
+
+__all__ = ('Cache', 'MobileDevice')
+
+
+class Cache(UAPIObject):
+    _endpoint_path = "settings/obj/cache"
+    can_post = False
+    can_delete = False
+
+
+class Ebook(UAPIObject):
+    _endpoint_path = "deployable/obj/ebook"
+    can_put = False
+    can_post = False
+    can_delete = False
+
+
+class MobileDevice(UAPIObject):
+    _endpoint_path = "inventory/obj/mobileDevice"
+
+
+class AlertNotification(UAPIObject):
+    _endpoint_path = "notifications/alerts"
+    can_put = False
+    can_post = False
+
+
+class PatchPolicy(UAPIObject):
+    _endpoint_path = "patch/obj/policy"
+
+
+class EnrollmentSettings(UAPIObject):
+    _endpoint_path = "settings/obj/enrollment"
+    can_post = False
+    can_delete = False
+
+
+class SelfServiceSettings(UAPIObject):
+    _endpoint_path = "settings/obj/selfservice"
+    can_post = False
+    can_delete = False
+
