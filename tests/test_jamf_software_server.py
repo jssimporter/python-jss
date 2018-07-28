843bc72f0e7141c2d480e4a150fa6962adac8a39
Mosen <mosen@users.noreply.github.com>
2018-07-28 23:47:01 +1000

    Create a new subdirectory for classic API object tests Move building tests into network_org dir Create new tests for departments and network segments

--- tests/test_jamf_software_server.py
+++ tests/test_jamf_software_server.py
@@ -34,26 +34,6 @@ class TestJSS(object):
         assert result is not None
         assert isinstance(result, QuerySet)
 
-    def test_new_building(self, j, etree_building):
-        fixture_building = jss.Building(j, etree_building)
-        fixture_building.save()
-        assert fixture_building.id is not None
-
-    def test_get_building(self, j, etree_building):
-        fixture_building = j.Building(etree_building.findtext('name'))
-        assert fixture_building is not None
-        assert fixture_building.id is not None
-
-    def test_update_building(self, j, etree_building):
-        fixture_building = j.Building(etree_building.findtext('name'))
-        fixture_building.find('name').text = 'Updated Fixture'
-        fixture_building.save()
-        fixture_building.delete()
-
-    def test_delete_building(self, j, etree_building):
-        fixture_building = j.Building(etree_building.findtext('name'))
-        fixture_building.delete()
-
     def test_scrape(self, j):
         #scrape_url = '/'
         scrape_url = 'legacy/packages.html?id=-1&o=c'
